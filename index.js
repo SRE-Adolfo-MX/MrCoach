fd742112dcd822e6f07d6d5261de4eb43aacb19a
Armando Rios <armandorg95@hotmail.com>
2021-12-17 05:44:05 -0600

    adding routes to index

--- index.js
+++ index.js
@@ -2,4 +2,24 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
-app.listen(port);
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello Trainer!</h1>");
+});
+
+app.get("/alumnos", (req, res) => {
+  res.send("<h1>Lista de Alumnos</h1>");
+});
+
+app.get("/rutinas", (req, res) => {
+  res.send("<h1>Lista de Rutinas</h1>");
+});
+
+app.get("/ejercicios", (req, res) => {
+  res.send("<h1>Lista de Ejercicios</h1>");
+});
+
+
+app.listen(port, () => {
+  console.log(`Listening on port: http://localhost:${port}`);
+});
